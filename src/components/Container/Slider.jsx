fe5f93e28d8f87ea3a749df06ca3713201c51538
Jorge Luis Guardo Romero <deliarosaro112@gmail.com>
2024-02-09 20:05:45 -0500

    Se corrigieron estilos de los slider, hay que anadirle la funcionalidad draggable y automatizarlo

--- src/components/Container/Slider.jsx
+++ src/components/Container/Slider.jsx
@@ -11,9 +11,9 @@ import slider3 from "../../img/slider3.jpg"
 
 const Slider = (props) => {
 
-    const [ isDrag, setIsDrag ] = useState(false)
-    const [ prPageX, setPrPageX ] = useState(0)
-    const [ prScroll, setPrScroll ] = useState(0)
+    const [isDrag, setIsDrag] = useState(false)
+    const [prPageX, setPrPageX] = useState(0)
+    const [prScroll, setPrScroll] = useState(0)
 
     const carouselRef = useRef(null);
 
@@ -40,22 +40,45 @@ const Slider = (props) => {
 
     return (
         <div className="wrapper">
-            <div className="carousel" 
-                onMouseDown={handleMouseD}
-                onMouseMove={handleMouseM}
-                onMouseUp={handleMouseU}
-                onTouchStart={handleMouseD}
-                onTouchMove={handleMouseM}
-                onTouchEnd={handleMouseU}
-                ref={carouselRef}
-                >
-                <img src={slider2}/>
-                <img src={slider1}/>
-                <img src={slider3}/>
-                <img src={slider2}/>
-                <img src={slider1}/>
+            <div className="carousel">
+                <div className="carousel-slider">
+                    <div className="carousel-slider-draggable">
+                        <div className="carousel-slider-track"
+                            style={{
+                                width: 5922,
+                                transform: "translate3d(-1950px, 0px, 0px)"
+                            }}
+                        // onMouseDown={handleMouseD}
+                        // onMouseMove={handleMouseM}
+                        // onMouseUp={handleMouseU}
+                        // onTouchStart={handleMouseD}
+                        // onTouchMove={handleMouseM}
+                        // onTouchEnd={handleMouseU}
+                        // ref={carouselRef}
+                        >
+                            <div className="track">
+                                <img src={slider1} />
+                            </div>
+                            <div className="track">
+                                <img src={slider2} />
+                            </div>
+                            <div className="track">
+                                <img src={slider3} />
+                            </div>
+                            <div className="track" style={{ transform: "scale(1)" }}>
+                                <img src={slider1} />
+                            </div>
+                            <div className="track">
+                                <img src={slider2} />
+                            </div>
+                            <div className="track">
+                                <img src={slider3} />
+                            </div>
+                        </div>
+                    </div>
+                </div>
             </div>
-        </div>
+        </div >
     )
 }
 
