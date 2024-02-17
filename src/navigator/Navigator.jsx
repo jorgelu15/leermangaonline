29476e7950cc26de4a6b24aa2a4f87604bb16114
Jorge Luis Guardo Romero <deliarosaro112@gmail.com>
2024-02-16 23:31:40 -0500

    se anadio carga diferida al cargar las paginas principales

--- src/navigator/Navigator.jsx
+++ src/navigator/Navigator.jsx
@@ -1,98 +1,191 @@
-import React from 'react'
+import React, { lazy, Suspense, useState, useEffect } from 'react'
 import { RouterProvider, createBrowserRouter } from 'react-router-dom'
+import { TailSpin } from 'react-loader-spinner'
 import ProtectedRoute from '../routers/ProtectedRoute'
 
-import Login from '../pages/Login'
-import Register from '../pages/Register'
-import Home from '../pages/Home'
-import Manga from "../pages/Manga"
-import Vermanga from '../pages/Vermanga'
-import Perfil from '../pages/Perfil'
-import Panel from '../pages/Panel'
-import Subirmanga from '../pages/Subirmanga'
-import Terms from '../pages/Terms'
-import Scanlation from '../pages/Scanlation'
-import GroupScans from '../pages/GroupScans'
-import Directory from '../pages/Directory'
+
 
 import routes from '../helpers/routes'
 
+const Home = lazy(async () => {
+  return Promise.all([
+    import('../pages/Home'),
+    new Promise((resolve) => setTimeout(resolve, 1500)),
+  ]).then(([moduleExports]) => moduleExports);
+});
+const Login = lazy(async () => {
+  return Promise.all([
+    import('../pages/Login'),
+    new Promise((resolve) => setTimeout(resolve, 1500)),
+  ]).then(([moduleExports]) => moduleExports);
+});
+const Register = lazy(async () => {
+  return Promise.all([
+    import('../pages/Register'),
+    new Promise((resolve) => setTimeout(resolve, 1500)),
+  ]).then(([moduleExports]) => moduleExports);
+});
+const Manga = lazy(async () => {
+  return Promise.all([
+    import("../pages/Manga"),
+    new Promise((resolve) => setTimeout(resolve, 1500)),
+  ]).then(([moduleExports]) => moduleExports);
+});
+const Vermanga = lazy(async () => {
+  return Promise.all([
+    import('../pages/Vermanga'),
+    new Promise((resolve) => setTimeout(resolve, 1500)),
+  ]).then(([moduleExports]) => moduleExports);
+});
+const Perfil = lazy(async () => {
+  return Promise.all([
+    import('../pages/Perfil'),
+    new Promise((resolve) => setTimeout(resolve, 1500)),
+  ]).then(([moduleExports]) => moduleExports);
+});
+const Panel = lazy(async () => {
+  return Promise.all([
+    import('../pages/Panel'),
+    new Promise((resolve) => setTimeout(resolve, 1500)),
+  ]).then(([moduleExports]) => moduleExports);
+});
+const Subirmanga = lazy(async () => {
+  return Promise.all([
+    import('../pages/Subirmanga'),
+    new Promise((resolve) => setTimeout(resolve, 1500)),
+  ]).then(([moduleExports]) => moduleExports);
+});
+const Terms = lazy(async () => {
+  return Promise.all([
+    import('../pages/Terms'),
+    new Promise((resolve) => setTimeout(resolve, 1500)),
+  ]).then(([moduleExports]) => moduleExports);
+});
+const Scanlation = lazy(async () => {
+  return Promise.all([
+    import('../pages/Scanlation'),
+    new Promise((resolve) => setTimeout(resolve, 1500)),
+  ]).then(([moduleExports]) => moduleExports);
+});
+const GroupScans = lazy(async () => {
+  return Promise.all([
+    import('../pages/GroupScans'),
+    new Promise((resolve) => setTimeout(resolve, 1500)),
+  ]).then(([moduleExports]) => moduleExports);
+});
+const Directory = lazy(async () => {
+  return Promise.all([
+    import('../pages/Directory'),
+    new Promise((resolve) => setTimeout(resolve, 1500)),
+  ]).then(([moduleExports]) => moduleExports);
+});
 
+
+const FallbackLoader = () => {
+  return (
+    <div style={{
+      display: "flex",
+      height: "100vh",
+      alignItems: "center",
+      justifyContent: "center"
+
+    }}>
+      <TailSpin
+        visible={true}
+        height="80"
+        width="80"
+        color="#4fa94d"
+        ariaLabel="tail-spin-loading"
+        radius="1"
+        wrapperStyle={{}}
+        wrapperClass=""
+        contentLoader={(props) => (
+          <div>
+            <TailSpin {...props} />
+            <p style={{ marginTop: "10px", fontSize: "16px", fontWeight: "bold", color: "#4fa94d" }}>Cargando...</p>
+          </div>
+        )}
+      />
+    </div>
+  )
+};
 
 const router = createBrowserRouter([
-    {
-      path: '/',
-      element: <Home />
-    },
-    {
-      path: routes.directory,
-      element: <Directory/>
-    },
-    {
-      path: routes.login,
-      element: <Login />
-    },
-    {
-      path: routes.register,
-      element: <Register />
-    },
-    {
-      path: routes.manga,
-      element: <Manga />
-    },
-    {
-      path: routes.vermanga,
-      element: <Vermanga />
-    },
-    {
-      path: routes.terms,
-      element: <Terms />
-    },
-    {
-      path: routes.scanlation,
-      element: <Scanlation />
-    },
-    {
-      path: routes.groups,
-      element: <GroupScans />
-    },
-    {
-      path: '/',
-      element: <ProtectedRoute/>,
-      children: [
-        {
-          path: routes.subirmanga,
-          element: <Subirmanga />
-        },
-      ]
-    },
-    {
-      path: '/',
-      element: <ProtectedRoute/>,
-      children: [
-        {
-          path: routes.panel,
-          element: <Panel />
-        },
-      ]
-    },
-    {
-      path: '/',
-      element: <ProtectedRoute/>,
-      children: [
-        {
-          path: routes.perfil,
-          element: <Perfil />
-        },
-      ]
-    },
+  {
+    path: '/',
+    element: <Suspense fallback={<FallbackLoader />}><Home /></Suspense>
+  },
+  {
+    path: routes.directory,
+    element: <Suspense fallback={<FallbackLoader />}><Directory /></Suspense>
+  },
+  {
+    path: routes.login,
+    element: <Suspense fallback={<FallbackLoader />}><Login /></Suspense>
+  },
+  {
+    path: routes.register,
+    element: <Suspense fallback={<FallbackLoader />}><Register /></Suspense>
+  },
+  {
+    path: routes.manga,
+    element: <Suspense fallback={<FallbackLoader />}><Manga /></Suspense>
+  },
+  {
+    path: routes.vermanga,
+    element: <Suspense fallback={<FallbackLoader />}><Vermanga /></Suspense>
+  },
+  {
+    path: routes.terms,
+    element: <Suspense fallback={<FallbackLoader />}><Terms /></Suspense>
+  },
+  {
+    path: routes.scanlation,
+    element: <Suspense fallback={<FallbackLoader />}><Scanlation /></Suspense>
+  },
+  {
+    path: routes.groups,
+    element: <Suspense fallback={<FallbackLoader />}><GroupScans /></Suspense>
+  },
+  {
+    path: '/',
+    element: <ProtectedRoute />,
+    children: [
+      {
+        path: routes.subirmanga,
+        element: <Suspense fallback={<FallbackLoader />}><Subirmanga /></Suspense>
+      },
+    ]
+  },
+  {
+    path: '/',
+    element: <ProtectedRoute />,
+    children: [
+      {
+        path: routes.panel,
+        element: <Suspense fallback={<FallbackLoader />}><Panel /></Suspense>
+      },
+    ]
+  },
+  {
+    path: '/',
+    element: <ProtectedRoute />,
+    children: [
+      {
+        path: routes.perfil,
+        element: <Suspense fallback={<FallbackLoader />}><Perfil /></Suspense>
+      },
+    ]
+  },
 ]);
+
+
 
 const Navigator = () => {
 
-    return (
-      <RouterProvider router={ router } />
-    )
+  return (
+    <RouterProvider router={router} />
+  )
 }
 
 export default Navigator;
