dd1b79db444ed562503613a0e6edf7502c2bb501
Reyselor <43126995+Reyselor@users.noreply.github.com>
2024-07-28 20:20:03 -0500

    header de vermanga configurado

--- src/components/Header/HeaderManga.jsx
+++ src/components/Header/HeaderManga.jsx
@@ -4,14 +4,17 @@ import routes from '../../helpers/routes';
 import { useSeries } from '../../hooks/useSeries';
 import { useEffect } from 'react';
 import { useText } from '../../hooks/useText';
+import { useGrupos } from '../../hooks/useGrupos';
 
 const Header = (props) => {
 
     const { paginacion, setPaginacion } = props
 
-    const { serie_uid, num_cap, id_capitulo } = useParams();
+    const { id_grupo, serie_uid, num_cap, id_capitulo } = useParams();
 
     const { serie, capitulo, getSerie, getCapitulo } = useSeries();
+
+    const { grupo, getGrupo } = useGrupos();
 
     const { reemplazarEspaciosConGuiones } = useText();
 
@@ -27,8 +30,12 @@ const Header = (props) => {
         }
     }, [id_capitulo]);
 
-    console.log(capitulo)
-    
+    useEffect(() => {
+        if(id_grupo){
+            getGrupo(id_grupo);
+        }
+    }, [id_grupo]);
+
     return (
         <header className="header-vermanga">
             <nav className="navbar">
@@ -39,7 +46,7 @@ const Header = (props) => {
                     <div className="info">
                         <div>
                             <p className='title' style={{ textTransform: "capitalize" }}><Link to={routes.manga + `/${serie_uid}/${reemplazarEspaciosConGuiones(serie?.nombre.toLowerCase())}`}>{serie?.nombre}</Link> Capitulo: {num_cap} {capitulo?.titulo}</p>
-                            <p>Subido por <a href="#">El club de askin</a></p>
+                            <p>Subido por <Link to={routes.scanlation + `/${id_grupo}`}>{grupo?.nombre}</Link></p>
                         </div>
                         <p>MANGA. El sentido de la lectura es de izquierda a derecha</p>
                         <button onClick={() => setPaginacion(!paginacion)}  style={{ padding: 5, backgroundColor: "#7bb9ff", border: "none", borderRadius: 3, fontWeight: "700", textTransform: "uppercase" }}>{paginacion ? "Paginado" : "cascada"}</button>
